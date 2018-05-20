b845f84ec8c11db126dea3c8be0615a762ef94f1
prateek44 <44beastmodeon@gmail.com>
2018-05-20 21:52:50 +0530

    shoppingListApp to add new items to the list ...

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 (function()
 {
-  'use strict';
+  //'use strict';
   angular.module("MultipleControllerApp",[])
-  .controller("MultipleControllerAppParentController",['$scope',MultipleControllerAppParentController])
-  .controller("MultipleControllerAppChildController",['$scope',MultipleControllerAppChildController]);
+  .controller("MultipleControllerAppParentController",MultipleControllerAppParentController)
+  .controller("MultipleControllerAppChildController",MultipleControllerAppChildController);
+  MultipleControllerAppParentController.$inject=['$scope'];
   function MultipleControllerAppParentController($scope)
   {
     $scope.parentValue=1;
     $scope.pc=this;
     $scope.pc.parentValue=1;
+    console.log("##### ",$scope.parentValue);
+    console.log("parent controller");
 
   }
+    MultipleControllerAppChildController.$inject=['$scope'];
   function MultipleControllerAppChildController($scope)
   {
+    console.log("child controller");
    console.log("$scope.parentValue",$scope.parentValue);
-   console.log("child $scope",$scope);
+   $scope.parentValue=33;
+   console.log("$scope.parentValue is masked and now child scope has it's own  $scope.parentValue property ",$scope.parentValue);
+   console.log("child $scopes",$scope);
   }
 })();
